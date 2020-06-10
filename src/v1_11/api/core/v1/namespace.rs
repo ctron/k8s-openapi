1cfbdf81c9fd878666cf4a0c964c4158724a1859
Jens Reimann <jreimann@redhat.com>
2020-06-11 00:01:44 +0200

    Revert "Add mutable methods to Metadata trait."
    This reverts commit 6c214de15e77e46e33766efdca21637bef68ae6b.

--- src/v1_11/api/core/v1/namespace.rs
+++ src/v1_11/api/core/v1/namespace.rs
@@ -568,14 +568,6 @@ impl crate::Metadata for Namespace {
     fn metadata(&self) -> Option<&<Self as crate::Metadata>::Ty> {
         self.metadata.as_ref()
     }
-
-    fn metadata_mut(&mut self) -> Option<&mut<Self as crate::Metadata>::Ty> {
-        self.metadata.as_mut()
-    }
-
-    fn set_metadata(&mut self, metadata: <Self as crate::Metadata>::Ty) {
-        self.metadata = Some(metadata);
-    }
 }
 
 impl<'de> serde::Deserialize<'de> for Namespace {
